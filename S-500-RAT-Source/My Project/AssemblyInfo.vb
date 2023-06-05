b6e1fa197e4bf41be2fafbc28fca6aef79ab8d3c
127.0.0.1 <125826136+MJMODZZ-V3@users.noreply.github.com>
2023-06-06 01:28:34 +0200

    👩‍💻 My Project 👩‍💻

---
+++ S-500-RAT-Source/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Diagnostics
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Runtime.Versioning
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyTitle("S500RAT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("S500RAT")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(False)>
+<Assembly: Guid("c7184297-0f2c-46d1-8404-0f649bbdda51")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
